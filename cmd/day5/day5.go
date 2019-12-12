37ebfb4ea20ce7ac3a4c42bf45d19570c66b61e6
Mathew Taylor <mat@mathewtaylor.co.uk>
2019-12-12 10:56:10 +0000

    Day 5, part 2.

--- cmd/day5/day5.go
+++ cmd/day5/day5.go
@@ -13,10 +13,26 @@ var opCodes = map[int]intcode.OperationDesc{
 	4: {1, intcode.Output},
 }
 
+
+var moreOpCodes = map[int]intcode.OperationDesc{
+	1: {3, intcode.Add},
+	2: {3, intcode.Multiply},
+	3: {1, intcode.SaveToPosition},
+	4: {1, intcode.Output},
+	5: {2, intcode.JumpIfTrue},
+	6: {2, intcode.JumpIfFalse},
+	7: {3, intcode.LessThan},
+	8: {3, intcode.Equals},
+}
+
+
 func main() {
 	rawIntCodes := files.Load("cmd/day5/input.txt", ",")
 	intCodes := converters.StringsToInts(rawIntCodes...)
 
 	prog := intcode.NewProgram(opCodes, intCodes)
+	prog.Execute()
+
+	prog = intcode.NewProgram(moreOpCodes, intCodes)
 	prog.Execute()
 }
--- internal/pkg/intcode/operations.go
+++ internal/pkg/intcode/operations.go
@@ -55,3 +55,47 @@ func Output(p *Program, modes []int) *Program {
 
 	return p
 }
+
+func JumpIfTrue(p *Program, modes []int) *Program {
+	if param := p.getIntCodeValue(p.pointer+1, modes[0]); param != 0 {
+		p.setPointer(p.getIntCodeValue(p.pointer+2, modes[1]))
+	} else {
+		p.setPointer(p.pointer + 3)
+	}
+
+	return p
+}
+
+func JumpIfFalse(p *Program, modes []int) *Program {
+	if param := p.getIntCodeValue(p.pointer+1, modes[0]); param == 0 {
+		p.setPointer(p.getIntCodeValue(p.pointer+2, modes[1]))
+	} else {
+		p.setPointer(p.pointer + 3)
+	}
+
+	return p
+}
+
+func LessThan(p *Program, modes []int) *Program {
+	if p.getIntCodeValue(p.pointer+1, modes[0]) < p.getIntCodeValue(p.pointer+2, modes[1]) {
+		p.setValue(p.pointer+3, 1)
+	} else {
+		p.setValue(p.pointer+3, 0)
+	}
+
+	p.setPointer(p.pointer + 4)
+
+	return p
+}
+
+func Equals(p *Program, modes []int) *Program {
+	if p.getIntCodeValue(p.pointer+1, modes[0]) == p.getIntCodeValue(p.pointer+2, modes[1]) {
+		p.setValue(p.pointer+3, 1)
+	} else {
+		p.setValue(p.pointer+3, 0)
+	}
+
+	p.setPointer(p.pointer + 4)
+
+	return p
+}
